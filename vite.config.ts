b3ca8a837bdf43815f1d0672388dd404ff64aa4e
Alexandros Kontos <alexiskon01@gmail.com>
2025-02-02 23:36:11 +0200

    update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/alexiskon/alexiskon.github.io.git',
+  base: '/alexiskon.github.io/',
   build: {
     outDir: 'docs'
   },
