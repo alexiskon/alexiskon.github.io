dab30bc0c97bd0e0008935a4d9cf961230d1d967
Alexandros Kontos <alexiskon01@gmail.com>
2025-02-02 22:37:16 +0200

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/alexiskon.github.io/',
   resolve: {
     alias: {
       '@images': path.resolve(__dirname, './src/assets/images'),
