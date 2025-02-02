af6df77c4c8564c22595284efcda42923297f4b7
Alexandros Kontos <alexiskon01@gmail.com>
2025-02-02 22:39:55 +0200

    docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   base: '/alexiskon.github.io/',
+  build: {
+    outDir: 'docs'
+  },
   resolve: {
     alias: {
       '@images': path.resolve(__dirname, './src/assets/images'),
