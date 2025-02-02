1f4146e742b1f366ee59df8f409680432efaefa2
Alexandros Kontos <alexiskon01@gmail.com>
2025-02-02 23:30:55 +0200

    try base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/alexiskon.github.io/',
+  base: 'https://github.com/alexiskon/alexiskon.github.io.git',
   build: {
     outDir: 'docs'
   },
